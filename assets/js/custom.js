4e51419eb2f273fef332fdcd5c07d18e4e26ca93
bugrauluyurt <bugra.u@gmail.com>
2016-02-13 19:29:19 +0200

    Minor changes in css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,22 @@
 /* Write here your custom javascript codes */
+jQuery(document).ready(function() {
+  	App.init();
+    LayerSlider.initLayerSlider();
+    StyleSwitcher.initStyleSwitcher();
+    OwlCarousel.initOwlCarousel();
+    OwlRecentWorks.initOwlRecentWorksV2();
+    $(".accordion-toggle").click(function () {
+        $(".widget_body_type").slideToggle("slow");
+        ($(this).children(".edit-toggle").text() === "+") ?  $(".edit-toggle").text("+") && $(this).children(".edit-toggle").text("-") : $(".edit-toggle").text("+");
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop()>0)
+         {
+            $('.header-above').fadeOut();
+         }
+        else
+         {
+          $('.header-above').fadeIn();
+         }
+    });
+});
