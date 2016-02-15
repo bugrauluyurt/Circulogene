c9938048b56380a267c2059caa6eb41194528840
bugrauluyurt <bugra.u@gmail.com>
2016-02-15 22:55:35 +0200

    Finished the services page.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,6 @@
 /* Write here your custom javascript codes */
 jQuery(document).ready(function() {
   	App.init();
-    LayerSlider.initLayerSlider();
-    StyleSwitcher.initStyleSwitcher();
-    OwlCarousel.initOwlCarousel();
-    OwlRecentWorks.initOwlRecentWorksV2();
     $(".accordion-toggle").click(function () {
         $(".widget_body_type").slideToggle("slow");
         ($(this).children(".edit-toggle").text() === "+") ?  $(".edit-toggle").text("+") && $(this).children(".edit-toggle").text("-") : $(".edit-toggle").text("+");
