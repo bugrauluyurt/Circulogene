194a7cd152d3282331928fff15fad2b881b4b8e2
bugrauluyurt <bugra.u@gmail.com>
2016-03-09 23:48:18 +0200

    SMTP email form implementation

--- send-mail-smtp.php
+++ send-mail-smtp.php
@@ -4,8 +4,9 @@ require 'PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 
-$mail->SMTPDebug = 2;                               // Enable verbose debug output
-$mail->Debugoutput = 'html';
+$mail->CharSet  = 'utf-8';
+// $mail->SMTPDebug = 2;                               // Enable verbose debug output
+// $mail->Debugoutput = 'html';
 
 // Assigning data from the $_POST array to variables     
 $name = $_POST['sender_name'];     
@@ -16,21 +17,22 @@ $message = $_POST['sender_message'];
 // Construct email subject     
 $subject = 'Mesajın geldigi musteri: ' . $name;       
 // Construct email body     
-$body_message = 'From: ' . $name . "\r\n";     
-$body_message .= 'E-mail: ' . $mail_from . "\r\n";     
-$body_message .= 'Phone: ' . $phone . "\r\n";     
-$body_message .= 'Message: ' . $message; 
+$body_message = '<b>From:</b> ' . $name . "\r\n" . "<br>";     
+$body_message .= '<b>E-mail:</b> ' . $mail_from . "\r\n" . "<br>";     
+$body_message .= '<b>Phone:</b> ' . $phone . "\r\n" . "<br>";     
+$body_message .= '<b>Message:</b> ' . "<br>" . $message . "<br>"; 
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.google.com';                      // Specify main and backup SMTP servers
+$mail->Host = 'smtp.yourdomain.com';                       // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'myemail@gmail.com';                // SMTP username
-$mail->Password = 'mypassword';                  // SMTP password
+$mail->Username = 'mymail@mydomain.com';         // SMTP username
+$mail->Password = 'mypassword';                     // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
+$mail->From = $mail_from;
 $mail->setFrom($mail_from);
-$mail->addAddress('myemail@gmail.com', 'John Doe');     // Add a recipient
+$mail->addAddress('mymail@mydomain.com');        // Add a recipient
 $mail->addReplyTo($mail_from, 'Information');
 
 $mail->isHTML(true);                                  // Set email format to HTML
@@ -38,11 +40,27 @@ $mail->isHTML(true);                                  // Set email format to HTM
 $mail->Subject = $subject;
 $mail->Body    = $body_message;
 
-if(!$mail->send()) {
-    echo 'Mesaj gonderilememistir.' . '<br>';
-    echo 'Eposta hatasi: ' . $mail->ErrorInfo;
-} else {
-    echo 'Mesaj gonderilmistir.';
-}
+
+// if(!$mail->send()) {
+//     echo 'Mesaj gonderilememistir.' . '<br>';
+//     echo 'Eposta hatasi: ' . $mail->ErrorInfo;
+// } else {
+//     echo 'Mesaj gonderilmistir.';
+// }
+
+if ($mail->send()){ ?>        
+        <script language="javascript" type="text/javascript">         
+        alert('Mesaj icin tesekkur ederiz. Tarafanizla en kisa surede iletisime gecilecektir.');                    
+    window.location = 'index.html';         
+        </script>    
+         <?php } else { ?>     
+         
+     <script language="javascript" type="text/javascript">         
+     alert('Mesaj gonderilememistir. Lutfen bilgi@globalgenetik.com adresine e-posta gonderiniz');            
+     window.location = 'index.html';     
+     </script>     
+<?php }
+
+
 		   
 ?>
